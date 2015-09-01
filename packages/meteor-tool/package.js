929097118f8adbe0d74676ced57e4b8ad632572e
Justin Rudio <justinjrudio@gmail.com>
2015-09-01 03:41:30 -0700

    Update version

--- packages/meteor-tool/package.js
+++ packages/meteor-tool/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'jrudio:meteor-tool',
   summary: "The Meteor command-line tool extended with desktop support",
-  version: '1.1.4_1'
+  version: '1.1.4_2'
 });
 
 Package.includeTool();
